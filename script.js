1dcbcac05a123972b7c65c6c0974fed96f96dd71
plhnt26-lgtm <plhnt26@gmail.com>
2026-07-16 06:28:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function saveProfile() {
+    const name = document.getElementById("fullname").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
 
+    alert("Profile Saved Successfully!");
+
+    console.log({
+        name,
+        email,
+        phone
+    });
+}
