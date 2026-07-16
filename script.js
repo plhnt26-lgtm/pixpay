2be39059d5de775697fadbe81081cda0bdcb2408
plhnt26-lgtm <plhnt26@gmail.com>
2026-07-16 11:20:21 +0700

    Add login function to handle user authentication

--- script.js
+++ script.js
@@ -371,3 +371,30 @@ function register(){
 
     window.location.href="login.html";
    }
+function login(){
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if(!user){
+        alert("No account found. Please register first.");
+        return;
+    }
+
+    if(email === user.email && password === user.password){
+
+        alert("Login Successful!");
+
+        localStorage.setItem("loggedIn","true");
+
+        window.location.href = "dashboard.html";
+
+    }else{
+
+        alert("Invalid Email or Password");
+
+    }
+
+       }
